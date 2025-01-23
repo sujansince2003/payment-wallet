1ad992c106830f511ea84b00a99ae45c4f832ea6
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-01-23 23:43:43 +0545

    added cors origin and user routes defined in app

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import cors from "cors";
+import Userroutes from "./routes/user.route.js";
 
 import cookieParser from "cookie-parser";
-import { env } from "./config/env.js";
 
 const app = express();
 app.use(express.json());
 
 app.use(
   cors({
-    origin: env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   })
 );
 
 app.use(cookieParser());
-
+app.use("/api/v1/user", Userroutes);
 export default app;
