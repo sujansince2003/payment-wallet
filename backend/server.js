a1a9c07364c9a5a8a63464667127d7ff549f0e7c
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-01-23 23:44:35 +0545

    setup entry point of our application

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
-import { env } from "./config/env.js";
 import { connectDB } from "./db/index.js";
 import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({});
 
 connectDB()
   .then(() => {
-    app.listen(env.PORT, () => {
-      console.log(`server is running on port ${env.PORT}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
     console.log("connection failed", error.message);
   });
-
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
