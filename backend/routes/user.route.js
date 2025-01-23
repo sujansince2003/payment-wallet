da026014333fcc1a80f0c45904f6e8c200d6d725
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-01-24 00:22:11 +0545

    route for handling login

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -13,6 +13,7 @@ const schema = z.object({
   password: z.string(),
 });
 
+//route for signup
 router.post("/signup", async (req, res) => {
   const validateInput = schema.safeParse(req.body);
   if (!validateInput.success) {
@@ -38,6 +39,33 @@ router.post("/signup", async (req, res) => {
   } catch (error) {
     console.log("e1", error);
   }
+});
+
+//route for login
+
+router.post("/login", async (req, res) => {
+  const validateInput = schema.safeParse(req.body);
+  if (!validateInput.success) {
+    return res.status(400).json({ message: validateInput.error.message });
+  }
+  const { username, password, email } = req.body;
+
+  //match documents where at least one of the specified conditions is true i.e find either with username or email
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    return res.status(403).json({ message: "user does not exist" });
+  }
+  const isPasswordvalid = await bcrypt.compare(password, user.password);
+  if (!isPasswordvalid) {
+    return res.status(400).json({ message: "Incorrect password" });
+  }
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+  return res.status(200).json({ message: "login successful", token });
 });
 
 export default router;
