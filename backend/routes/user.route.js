901feb6f686f5cf4d3cba42c83e56f833f755c88
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-01-26 02:20:36 +0545

    defined user route to edit the user info and filter route to get userinfo  throung filtering with keywords

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import mongoose from "mongoose";
 import { User } from "../models/User.model.js";
 import { z } from "zod";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import authMiddlware from "./middlewares/auth.middleware.js";
 
 const router = Router();
 console.log(process.env.JWT_SECRET);
@@ -30,7 +30,7 @@ router.post("/signup", async (req, res) => {
     const user = new User({ email, username, password: hashPassword });
     await user.save();
     const jwtToken = jwt.sign(
-      { username: user.username },
+      { username: user.username, userID: user._id },
       process.env.JWT_SECRET
     );
     return res
@@ -63,9 +63,77 @@ router.post("/login", async (req, res) => {
     return res.status(400).json({ message: "Incorrect password" });
   }
 
-  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+  const token = jwt.sign(
+    { username: user.username, userID: user._id },
+    process.env.JWT_SECRET
+  );
 
-  return res.status(200).json({ message: "login successful", token });
+  return res.status(200).json({ message: "login successful", user, token });
+});
+
+//route to  update user details
+
+router.put("/update", authMiddlware, async (req, res) => {
+  const validateInput = schema.safeParse(req.body);
+  if (!validateInput.success) {
+    return res.status(400).json({ message: "invalid input" });
+  }
+  const { email, username, password } = req.body;
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    return res.status(403).json({ message: "user doesnot exist" });
+  }
+  const HashedPassword = await bcrypt.hash(password, 10);
+  const updateUser = await User.findOneAndUpdate(
+    {
+      _id: req.userID,
+    },
+    {
+      username,
+      email,
+      password: HashedPassword,
+    },
+    { new: true }
+  );
+  updateUser
+    ? res.status(200).json({ message: "user updated successfully", updateUser })
+    : res.status(400).json({ message: "user not updated" });
+});
+
+//route to filter
+router.get("/filter", async (req, res) => {
+  const searchQuery = req.query;
+
+  const searchSchema = z.object({
+    fname: z.string().optional(),
+    email: z.string().email().optional(),
+  });
+  const validateSearchParams = searchSchema.safeParse(searchQuery);
+  if (!validateSearchParams.success) {
+    return res.status(400).json({ message: "invalid  search input" });
+  }
+  const { fname, email } = searchQuery;
+
+  const query = {};
+
+  if (fname) {
+    query.username = { $regex: fname, $options: "i" };
+  }
+  if (email) {
+    query.email = { $regex: email, $options: "i" };
+  }
+
+  const users = await User.find(query);
+
+  if (users.length === 0) {
+    return res.status(404).json({ message: "No users found" });
+  }
+
+  return res.status(200).json({
+    users,
+  });
 });
 
 export default router;
