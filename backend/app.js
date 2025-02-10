d5a8eaa65ab2ee9b5a0990fb376b29b4e33d8c23
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-02-10 23:54:59 +0545

    defined cors to connect Fe to BE

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,14 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: "http://localhost:5173",
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
 );
+
+app.options("*", cors()); //ensures the server properly responds to preflight requests, preventing CORS errors.
 
 app.use(cookieParser());
 app.use("/api/v1/user", UserRoutes);
